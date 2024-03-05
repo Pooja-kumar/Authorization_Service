8df67e2af2f4f82126ab254f2e3f5f5e02b1e9c7
Pooja <poojaokmoku1999@gmail.com>
2024-03-05 07:54:43 +0530

    Add project files.

---
+++ Models/UserContext.cs
@@ -0,0 +1,26 @@
+﻿//using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationService.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext() { }
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-S1MBOP8B\SQLEXPRESS;Initial Catalog=AuthorizationDb;User ID=sa;Password=pass@2024");
+            }
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
